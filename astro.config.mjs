2923cecd3f03329ab2974ed1e24e126644ba2518
chinaCCTVfilm <limingvv789@gmail.com>
2026-03-25 16:08:01 +0800

    deploy film site - 03/25/2026 Wed 16:08:01.83

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-﻿import { defineConfig } from 'astro/config';
-import tailwind, tailwindcss from '@tailwindcss/vite';
+﻿// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwind(), tailwindcss()],
+    plugins: [tailwindcss()],
   },
   output: 'static',
-  site: 'https://v300-0nz.pages.dev/',   // 改成你的真实自定义域名
+  site: 'https://v300-0nz.pages.dev',   // 改成你的自定义域名
   trailingSlash: 'ignore',
 });
